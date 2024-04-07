37a7a29ad535cabb936f51bf184b3fcfb9130d67
Akshatnachhola <akshatnachhola1997@gmail.com>
2024-04-07 15:34:05 +0530

    First Commit

---
+++ Models/DTO/UpdateCategoryRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CodePulse.API.Models.DTO
+{
+    public class Update_CategoryRequestDto
+    {
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
